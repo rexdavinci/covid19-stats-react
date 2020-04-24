696c16a380fa174fe2a33d7c27fa3c1aecfe65e0
Cornelius Okeke <okekecornelius@gmail.com>
2020-04-24 23:58:24 +0100

    added font awesome and global variable

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php';
+
+export const httpHeaders = {
+    headers: {
+        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+        'x-rapidapi-key': 'fbf1b04851mshc7ae899ebcfdeacp110ecajsncb3dcc3a0692'
+    }
+};
